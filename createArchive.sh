4f33029c6e0c1500aefd6a3853638064560c989c
rylyade1 <rylyade@protonmail.com>
2017-04-05 15:06:25 +0200

    another test

--- createArchive.sh
+++ createArchive.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-<<<<<<< HEAD
 DIR="target/config"
 mkdir -p $DIR
 
@@ -14,24 +13,3 @@ cd target/
 
 zip $(echo $NAME).zip $(echo $WAR_FILE) config/*
 tar -czf $(echo $NAME).tar.gz $(echo $WAR_FILE) config/*
-
-=======
-cd target/
-
-WAR_FILE="$(ls | egrep .*\.war$)"
-NAME="$(echo $WAR_FILE | egrep -oh '(.*)[^\.war]')"
-
-echo "$WAR_FILE"
-echo "$NAME"
-
-
-
-mkdir config
-
-cp ../src/main/resources/config/application.yml config/
-cp ../src/main/resources/config/application-prod.yml config/
-cp ../src/main/resources/config/application-dev.yml config/
-
-zip $(echo $NAME).zip $(echo $WAR_FILE) config/*
-tar -czf $(echo $NAME).tar.gz $(echo $WAR_FILE) config/*
->>>>>>> 10ff92435b2c06c52dd0ea08b34878a4f0accbf5
